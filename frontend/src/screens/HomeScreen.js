a7b0c7b96837b5ff604f4df1118c5a1cdc735e1b
FazaAkila <79125027+FazaAkila@users.noreply.github.com>
2021-09-22 20:35:00 +0700

    update cart

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,7 @@
+import "./HomeScreen.css";
+
+const HomeScreen = () => {
+    return <div className="HomeScreen">Home Screen</div>;
+};
+
+export default HomeScreen;
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,7 @@
+import "./ProductScreen.css";
+
+const ProductScreen = () => {
+    return <div className="ProductScreen">Product Screen</div>;
+};
+
+export default ProductScreen;
